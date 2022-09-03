45eb9fa5cb53b11107a7183cb420985b37b5e4a7
pandeykushagra51 <61946373+pandeykushagra51@users.noreply.github.com>
2022-09-03 19:19:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const PORT = process.env.PORT || 3000;
 
 
 
+
+
+/**
+ * call youtube API at interval of every 10 second
+ */
+setInterval(()=>{
+    callYoutubeApi();
+},10000);
+
+
 server.app.listen(PORT, ()=>{
     console.log('Server started and Listening on port :',PORT);
 })
